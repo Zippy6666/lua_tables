c134b8e8dfe8b94976dc31dfc914364eb9b03bd9
Zippy6666 <simperzippy@gmail.com>
2023-10-24 18:19:16 +0200

    revamp

--- __init__.py
+++ __init__.py
@@ -1,9 +1,2 @@
-from ._tbl_class import Tbl
+from ._tbl_class import Table
 from ._tbl_library import table
-
-def main():
-    t = Tbl(a=1)
-    print(t)
-
-if __name__ == "__main__":
-    main()
--- _tbl_class.py
+++ _tbl_class.py
@@ -1,59 +1,99 @@
-class Tbl():
-    def __init__( self, **kwargs ):
-        """ Returns a lua table object """
-        self._contents = {}
+from typing import Any
 
+
+class Table:
+    def __init__(self, *args:Any, dict:dict={}, **kwargs: dict[str, Any]):
+        """Returns a lua table object. Currently has:
+            - Key, value pairs [(string, int, float), Any]
+            - Setting values with item assignment
+            - Setting values by setting attributes
+            - Removing values by setting them to None
+            - Does not raise IndexError, KeyError, or AttributeError, it instead returns None
+            - Lenght
+        """
+
+        self._num_dict = {}
+
+        # Arguments
+        for i, v in enumerate(args, 1):
+            self._num_dict[i] = v
+
+        # Keyword arguments
         for k, v in kwargs.items():
-            self._setitem( k, v )
+            setattr(self, k , v)
 
-    def __getitem__( self, index ) -> any:
-        if not index in self._contents:
-            return None
-        
-        return self._contents[index]
+        # Dictionary argument
+        for k, v in dict.items():
+            self[k] = v
 
-    def __setitem__( self, index, value ):
-        self._setitem( index, value, True )
 
-    def _setitem( self, index, value, setAttr=False ):
-        if value is None:
-            if index in self._contents:
-                del self._contents[ index ]
-            return
+    def _key_type_check(func):
+        """ Raise error if key is not int, float or str"""
 
-        self._contents[index] = value
+        def wrapper( self, key, *args ):
+            if not isinstance(key, (int, float, str)):
+                raise TypeError("Table index must be a string or a number!")
+            return func(self, key, *args)
 
-        if setAttr is True:
-            setattr(self, index, value, setItem=False)
+        return wrapper
 
-    def __repr__( self ):
-        string = "{"
-        kV = self._contents.items()
+    @_key_type_check
+    def __setitem__(self, key, value):
+        if isinstance(key, str):
 
-        for i, (k, v) in enumerate( kV ):
-            string += f"{k} = {v}"
+            if value is None:
+                delattr(self, key)
+            else:
+                setattr(self, key, value)
 
-            if i < len(kV)-1:
-                string += ", "
-        
-        string += "}"
-        return string
+        elif isinstance(key, (int, float)):
 
-    def __getattr__( self, attr ):
-        if attr in self._contents:
-            return self._contents[attr]
+            if value is None:
+                del self._num_dict[key]
+            else:
+                self._num_dict[key] = value
+            
+    @_key_type_check
+    def __getitem__(self, key):
+        if isinstance(key, str):
 
-    def __setattr__( self, attr, value, setItem=True ):
-        if setItem is True and attr != "_contents":
-            self._setitem( attr, value )
+            if hasattr(self, key):
+                return getattr(self, key)
+            else:
+                return None
 
+        elif isinstance(key, (int, float)):
+
+            if key in self._num_dict:
+                return self._num_dict[key]
+            else:
+                return None
+    
+    def __setattr__(self, attr, value):
         if value is None:
             delattr(self, attr)
         else:
-            super().__setattr__( attr, value )
+            super().__setattr__(attr, value)
+    
+    def __getattr__(self, attr):
+        try:
+            return super().__getattr__(attr)
+        except AttributeError:
+            return None
+        
+    def __len__(self):
+        n = 0
+        for k, v in self._num_dict.items():
+            print(k, v)
+        return 0
+    
+    def __repr__(self):
+        return f"table: {hex(id(self))}"
 
-    def __iter__( self ):
-        return iter( self._contents.items() )
+def main():
+    t = Table("dog", 1, 2, 3, "fart")
+    print(f"#{len(t)}")
 
-    def __len__( self ):
-        return len( self._contents )
+
+if __name__ == "__main__":
+    main()
